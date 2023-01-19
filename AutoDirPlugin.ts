1a5652806d8f087015ad5bc0a73e8ad8c118c6b3
Erez Shermer <erezshermer@gmail.com>
2023-01-19 11:25:19 +0200

    Refresh now works properly

--- AutoDirPlugin.ts
+++ AutoDirPlugin.ts
@@ -3,6 +3,9 @@ import {ViewPlugin, ViewUpdate, Decoration, PluginValue, DecorationSet, EditorVi
 const dirAutoDecoration = Decoration.line({attributes: {dir: "auto"}});
 const dirDefaultDecoration = Decoration.line({attributes: {dir: ''}});
 
+// Based on this example:
+// https://codemirror.net/try/?c=aW1wb3J0IHtFZGl0b3JWaWV3LCBiYXNpY1NldHVwfSBmcm9tICJjb2RlbWlycm9yIgppbXBvcnQge2phdmFzY3JpcHR9IGZyb20gIkBjb2RlbWlycm9yL2xhbmctamF2YXNjcmlwdCIKaW1wb3J0IHtWaWV3UGx1Z2luLCBEZWNvcmF0aW9ufSBmcm9tICJAY29kZW1pcnJvci92aWV3IgoKY29uc3QgZGlyQXV0byA9IERlY29yYXRpb24ubGluZSh7YXR0cmlidXRlczoge2RpcjogImF1dG8ifX0pCgpjb25zdCBhdXRvTGluZURpciA9IFZpZXdQbHVnaW4uZGVmaW5lKHZpZXcgPT4gKHsKICBkZWNvOiB2aWV3cG9ydExpbmVEZWNvKHZpZXcsIGRpckF1dG8pLAogIHVwZGF0ZSh1cGRhdGUpIHsKICAgIGlmICh1cGRhdGUuZG9jQ2hhbmdlZCB8fCB1cGRhdGUudmlld3BvcnRDaGFuZ2VkIHx8IHVwZGF0ZS5oZWlnaHRDaGFuZ2VkKQogICAgICB0aGlzLmRlY28gPSB2aWV3cG9ydExpbmVEZWNvKHVwZGF0ZS52aWV3LCBkaXJBdXRvKQogIH0KfSksIHsKICBkZWNvcmF0aW9uczogcCA9PiBwLmRlY28KfSkKCmZ1bmN0aW9uIHZpZXdwb3J0TGluZURlY28odmlldywgZGVjbykgewogIHJldHVybiBEZWNvcmF0aW9uLnNldCh2aWV3LnZpZXdwb3J0TGluZUJsb2Nrcy5tYXAobCA9PiBkZWNvLnJhbmdlKGwuZnJvbSkpKQp9Cgp3aW5kb3cudmlldyA9IG5ldyBFZGl0b3JWaWV3KHsKICBkb2M6ICdjb25zb2xlLmxvZygiSGVsbG8gd29ybGQiKVxu/0UG/zEG/y0G/ygG/ycGIP8oBv8nBv9EBv85Bv8nBv9EBv9FBlxuJywKICBleHRlbnNpb25zOiBbCiAgICBiYXNpY1NldHVwLAogICAgamF2YXNjcmlwdCgpLAogICAgYXV0b0xpbmVEaXIsCiAgICBFZGl0b3JWaWV3LnBlckxpbmVUZXh0RGlyZWN0aW9uLm9mKHRydWUpCiAgXSwKICBwYXJlbnQ6IGRvY3VtZW50LmJvZHkKfSkK
+
 class AutoDirectionPlugin implements PluginValue {
 	decorations: DecorationSet;
 	active = true;
@@ -21,6 +24,8 @@ class AutoDirectionPlugin implements PluginValue {
 
 	setActive(active: boolean, view: EditorView) {
 		this.active = active;
+		this.decorations = this.buildDecorations(view);
+		view.dispatch();
 	}
 
 	buildDecorations(view: EditorView) {
